ecffa95af8a2f6725f88c378be6b09f4d897604b
chengluyu <chengluyu@live.cn>
2016-01-08 21:59:16 +0800

    Complete `EvaluationStack` class.

--- src/runtime/EvaluationStack.java
+++ src/runtime/EvaluationStack.java
@@ -5,14 +5,29 @@ import java.util.Stack;
 public class EvaluationStack {
 
 	public EvaluationStack() {
-		stack_ = new Stack<RuntimeObject>();
+		stack_ = new Stack<StackFrame>();
+		enter();
 	}
 	
-	private Stack<RuntimeObject> stack_;
+	private Stack<StackFrame> stack_;
+	private StackFrame current_;
+	
+	public void enter() {
+		current_ = new StackFrame();
+		stack_.push(current_);
+	}
+	
+	public void leave() {
+		stack_.pop();
+		current_ = stack_.peek();
+	}
 	
 	public void push(RuntimeObject obj) {
-		stack_.push(obj);
-		
+		current_.push(obj);
+	}
+	
+	public RuntimeObject pop() {
+		return current_.pop();
 	}
 
 }
