f2a1fd769baceab77a52093cd1d2e69b5ab31279
chengluyu <chengluyu@live.cn>
2016-01-08 21:53:09 +0800

    Complete `StackFrame`.

--- src/runtime/StackFrame.java
+++ src/runtime/StackFrame.java
@@ -1,9 +1,25 @@
 package runtime;
 
+import java.util.Stack;
+
 public class StackFrame {
 
 	public StackFrame() {
-		// TODO Auto-generated constructor stub
+		stack_ = new Stack<RuntimeObject>();
+	}
+	
+	private Stack<RuntimeObject> stack_;
+	
+	public void push(RuntimeObject obj) {
+		stack_.push(obj);
+	}
+	
+	public RuntimeObject pop() {
+		return stack_.pop();
+	}
+	
+	public boolean empty() {
+		return stack_.isEmpty();
 	}
 
 }
